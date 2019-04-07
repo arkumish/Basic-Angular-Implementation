5d10cfc2d0e306cf00c343407bd62d64719d2924
armish24 <arpit.cse24@gmail.com>
2019-04-07 07:47:46 +0530

    added sevice

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs'; //hold data  return from api
+import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
+  
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getusers().subscribe(
+      data => this.users$ = data 
+    );
   }
 
 }
